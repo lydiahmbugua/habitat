9ff21c9eb29bab8af87fc02c3992be579b050cef
lydiahw <haidylwanjiku@gmail.com>
2026-08-11 16:27:58 +0300

    styles: change logout btn style

--- src/components/Navigation/AppNav.jsx
+++ src/components/Navigation/AppNav.jsx
@@ -36,8 +36,8 @@ function AppNav() {
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
+              width="20"
+              height="20"
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
@@ -49,6 +49,7 @@ function AppNav() {
               <path d="m16 17 5-5-5-5" />
               <path d="M21 12H9" />
               <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
+              <circle cx="12" cy="12" r="3" />
             </svg>
           </button>
 
